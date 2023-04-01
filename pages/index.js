ef017dd772acc56a653736d3fadabae46a88d5e7
squink13 <squireink13@gmail.com>
2023-03-31 19:45:26 -0600

    Update header

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Text h1 css={{ textGradient: "45deg, $blue600 -10%, $pink600 100%" }}>
-        {"Squink's Epic Gacha Tournament"}
+        {"Squink's Epic Gacha Showdown"}
       </Text>
       <Spacer y={1} />
     </>
