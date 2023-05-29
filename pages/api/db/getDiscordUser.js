a7716a425b905d42b0c28e2fc8a791787a9cac95
squink13 <squireink13@gmail.com>
2023-05-29 07:49:31 -0600

    properly handle errors

--- pages/api/db/getDiscordUser.js
+++ pages/api/db/getDiscordUser.js
@@ -3,27 +3,37 @@ import { XataClient } from "@/xata";
 const xata = new XataClient();
 
 const handler = async (req, res) => {
-  const { userId } = req.body;
-  const record = await xata.db.nextauth_accounts
-    .select(["access_token"])
-    .filter({ provider: "discord", "user.id": userId })
-    .getMany();
+  try {
+    const { userId } = req.body;
+    const record = await xata.db.nextauth_accounts
+      .select(["access_token"])
+      .filter({ provider: "discord", "user.id": userId })
+      .getMany();
 
-  const url = "https://discord.com/api/users/@me";
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${record[0].access_token}`,
-    },
-  });
+    // Check if the access_token is null before proceeding
+    if (!record[0] || !record[0].access_token) {
+      throw new Error("Access token is null");
+    }
 
-  if (!response.ok) {
-    res.status(response.status).json({ error: "Failed to fetch from Discord API" });
-    return;
+    const url = "https://discord.com/api/users/@me";
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${record[0].access_token}`,
+      },
+    });
+
+    if (!response.ok) {
+      res.status(response.status).json({ error: "Failed to fetch from Discord API" });
+      return;
+    }
+
+    const user = await response.json();
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
   }
-
-  const user = await response.json();
-
-  res.status(200).json(user);
 };
 
 export default handler;
--- pages/api/db/getOsuUser.js
+++ pages/api/db/getOsuUser.js
@@ -4,20 +4,30 @@ import { Client } from "osu-web.js";
 const xata = new XataClient();
 
 const handler = async (req, res) => {
-  const { userId } = req.body;
-  const record = await xata.db.nextauth_accounts
-    .select(["access_token"])
-    .filter({ provider: "osu", "user.id": userId })
-    .getMany();
+  try {
+    const { userId } = req.body;
+    const record = await xata.db.nextauth_accounts
+      .select(["access_token"])
+      .filter({ provider: "osu", "user.id": userId })
+      .getMany();
 
-  const client = new Client(record[0].access_token);
-  const user = await client.users.getSelf({
-    urlParams: {
-      mode: "osu",
-    },
-  });
+    // Check if the access_token is null before proceeding
+    if (!record[0] || !record[0].access_token) {
+      throw new Error("Access token is null");
+    }
 
-  res.status(200).json(user);
+    const client = new Client(record[0].access_token);
+    const user = await client.users.getSelf({
+      urlParams: {
+        mode: "osu",
+      },
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export default handler;
