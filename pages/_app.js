9b75c3c98fc48c791977af4a9ec387ee341abaef
squink13 <squireink13@gmail.com>
2023-06-05 21:55:13 -0600

    Add warning message for firefox users

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import NavigationBar from "@/components/NavigationBar";
 import { createTheme, NextUIProvider, styled } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const lightTheme = createTheme({
   type: "light",
@@ -22,6 +24,23 @@ const Box = styled("div", {
 });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  useEffect(() => {
+    // This will run when the component mounts
+    // and will add a toast if the browser is Firefox.
+    var isFirefox = typeof InstallTrigger !== "undefined";
+    if (isFirefox) {
+      toast("Warning! Firefox is not supported at this time.", {
+        duration: 10000,
+        icon: "⚠️",
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+    }
+  }, []);
+
   return (
     <>
       <NextThemesProvider
@@ -34,6 +53,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
       >
         <NextUIProvider>
           <SessionProvider session={session}>
+            <Toaster />
             <NavigationBar />
             <Box
               css={{
